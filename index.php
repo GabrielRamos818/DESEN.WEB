154811ffe0071519d4be43f318f1e763b30a85d6
GabrielRamos818 <gabrieljoaqsouza@gmail.com>
2021-11-12 15:36:11 -0400

    Início projeto!

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Seja bem vindo!</title>
+
+    <!-- Fonte do Google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- CSS da aplicação --> 
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Script da aplicação --> 
+    <script src="script.js"></script>
+
+</head>
+<body>
+
+<!-- CABEÇALHO -->
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+                <a href="/" class="navbar-brand">
+                   <img src="img/My Post.png" alt="">    
+            </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Meus produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contato</a>
+          </li>
+            </ul>
+      </div>
+    </nav>
+</header>
+<!-- CORPO E CADASTRO DO PRODUTO -->
+
+<!-- RODAPÉ --> 
+    
+</body>
+</html>
